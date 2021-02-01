b44c17335541308bc6c4f7b72bdb68b478785008
Enxo8 <enzox844@gmail.com>
2021-02-01 13:16:37 +0100

    codigo

--- src/mainApp/MainApp.java
+++ src/mainApp/MainApp.java
@@ -6,6 +6,8 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		new MainView().main();;
+//		String dificultadString = (String) JOptionPane.showInputDialog(null, "Nivel de dificultad?", "Selection",
+//				JOptionPane.DEFAULT_OPTION, null, diffi, "0");
 
 	}
 
